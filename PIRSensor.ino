b15ac7a8f647f4d1d63f16f5813bb48e899b7115
Zhong Ming Tan <31769920+ming0520@users.noreply.github.com>
2023-04-24 21:18:15 +0800

    Add DS3231 feature
    migrate arduino uno to esp32s0

--- PIRSensor.ino
+++ PIRSensor.ino
@@ -14,17 +14,22 @@ uint32_t start, stop;
 uint8_t  ar[4];
 
 // Define the PIR sensor input pin
-int pirSensorPin = A0;
+int pirSensorPin = A7;
 
 // Define Buzzer output pin
 int buzzerPin = 8;
 
-void triggerAlarm(){
-  // Buzzer
-  tone(buzzerPin, 1000); // Turn on the buzzer with a frequency of 1000Hz
-  delay(1000); // Wait for 1 second
-  noTone(buzzerPin); // Turn off the buzzer
-}
+RTC_DS3231 rtc;
+char t[32];
+
+// Arduino Code
+// void triggerAlarm(){
+//   // Buzzer
+//   tone(buzzerPin, 1000); // Turn on the buzzer with a frequency of 1000Hz
+//   delay(1000); // Wait for 1 second
+//   noTone(buzzerPin); // Turn off the buzzer
+// }
+// End Arduino Code
 
 void setup() {
   // Initialize serial communication for debugging
@@ -47,8 +52,13 @@ void setup() {
   seg.displayOn();
   seg.displayColon(false);
 
+  // Arduino Code
   // Set the buzzer pin as output
-  pinMode(buzzerPin, OUTPUT); 
+  // pinMode(buzzerPin, OUTPUT); 
+  // End Arduino Code
+
+// for DS3231
+  rtc.begin();
 }
 
 void loop() {
@@ -58,6 +68,11 @@ void loop() {
   Serial.print(pirSensorPin);
   Serial.println();
 
+  DateTime now = rtc.now();
+  sprintf(t, "%02d:%02d:%02d %02d/%02d/%02d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());  
+  Serial.print(F("Date/Time: "));
+  Serial.println(t);
+
   // Convert the analog value to a digital value
   if (pirSensorValue > 512) {
     pirSensorValue = 1;
@@ -66,6 +81,8 @@ void loop() {
   } else { 
     pirSensorValue = 0;
   }
+
+  
 
  // seg.displayTime(hours, minutes);
   seg.displayColon(1);
