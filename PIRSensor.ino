066641729581da69e2e8533a83f476bcd2ce397b
Zhong Ming Tan <31769920+ming0520@users.noreply.github.com>
2023-04-29 22:08:29 +0800

    Done LCD display

--- PIRSensor.ino
+++ PIRSensor.ino
@@ -8,11 +8,17 @@
 // https://circuitdigest.com/microcontroller-projects/interfacing-ds3231-rtc-with-arduino-and-diy-digital-clock
 #include <RTClib.h>
 RTC_DS3231 rtc;
+char d[32];
 char t[32];
 
 // Install Adafruit LiquidCrystal by Adafruit from Manage Libraries
-#include "Adafruit_LiquidCrystal.h"
-Adafruit_LiquidCrystal lcd(0);
+// #include "Adafruit_LiquidCrystal.h"
+// Adafruit_LiquidCrystal lcd(0);
+
+
+// Download LiquidCrystal I2C by Frank de
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2); 
 
 // // Define 7 segment display
 // HT16K33  seg(0x70);
@@ -35,10 +41,11 @@ int buzzerPin = 8;
 // }
 // End Arduino Code
 
+DateTime lastTriggerDate;
 void setup() {
   // Initialize serial communication for debugging
   Serial.begin(9600);
-
+  lcd.backlight();
   // Set the PIR sensor pin as an input
   pinMode(pirSensorPin, INPUT);
 
@@ -64,18 +71,23 @@ void setup() {
 
 // for DS3231
   rtc.begin();
+  lcd.init();
+  lastTriggerDate = rtc.now();
 
 }
 
+
 void loop() {
   // Read the analog value from the PIR sensor
+  lcd.clear();
   int pirSensorValue = analogRead(pirSensorPin);
   Serial.print("Sensor value: ");
   Serial.print(pirSensorPin);
   Serial.println();
 
-  DateTime now = rtc.now();
-  sprintf(t, "%02d:%02d:%02d %02d/%02d/%02d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());  
+  // DateTime now = rtc.now();
+  sprintf(d,"%02d/%02d/%02d",lastTriggerDate.day(), lastTriggerDate.month(), lastTriggerDate.year());
+  sprintf(t, "%02d:%02d:%02d", lastTriggerDate.hour(), lastTriggerDate.minute(), lastTriggerDate.second());  
   Serial.print(F("Date/Time: "));
   Serial.println(t);
 
@@ -83,13 +95,19 @@ void loop() {
   if (pirSensorValue > 512) {
     pirSensorValue = 1;
     Serial.println("Motion Detected!");
-    // triggerAlarm();
+    lastTriggerDate = rtc.now();
+    triggerAlarm();
   } else { 
     pirSensorValue = 0;
   }
-
+  lcd.setCursor(0, 0);
+  lcd.print(d);
+  lcd.setCursor(0, 1);
+  lcd.print(t);
   // Wait for a short time to prevent rapid triggering of the sensor
   delay(500);
+  lcd.clear();
+
 }
 
 
